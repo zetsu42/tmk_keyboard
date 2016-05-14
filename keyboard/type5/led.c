8ba89d5d4a5ea1c19c6f8cd0fb3d9354b6f30bb7
zetsu42 <zetsu42@users.noreply.github.com>
2016-05-14 22:49:22 +0200

    fix led

--- keyboard/type5/led.c
+++ keyboard/type5/led.c
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 void led_set(uint8_t usb_led)
 {
-    /*if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
         // output low
         DDRB |= (1<<0);
         PORTB &= ~(1<<0);
@@ -30,17 +30,17 @@ void led_set(uint8_t usb_led)
         PORTB &= ~(1<<1);
         DDRC |= (1<<6);
         PORTC &= ~(1<<6);
-        DDRB &= ~(1<<3);
+        DDRB |= (1<<3);
         PORTB &= ~(1<<3);
     } else {
         // Hi-Z
-        DDRB &= ~(1<<0);
-        PORTB &= ~(1<<0);
-        DDRB &= ~(1<<1);
-        PORTB &= ~(1<<1);
-        DDRC &= ~(1<<6);
-        PORTC &= ~(1<<6);
-        DDRC &= ~(1<<3);
-        PORTC &= ~(1<<3);
-    }*/
+        DDRB |= (1<<0);
+        PORTB |= (1<<0);
+        DDRB |= (1<<1);
+        PORTB |= (1<<1);
+        DDRC |= (1<<6);
+        PORTC |= (1<<6);
+        DDRC |= (1<<3);
+        PORTC |= (1<<3);
+    }
 }
